548073c58ed3c0713f57e27096a56ac30f4dda6a
Fabien Vauchelles <fabien@vauchelles.com>
2016-04-10 17:20:06 +0200

    feat(release): add 1.3 release config

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name='scrapoxy',
     packages=['scrapoxy'],
-    version='1.2',
+    version='1.3',
     description='Python API for Scrapoxy and Scrapy',
     author='Fabien Vauchelles',
     author_email='fabien@vauchelles.com',
     url='https://github.com/fabienvauchelles/scrapoxy-python-api',
-    download_url='https://github.com/fabienvauchelles/scrapoxy-python-api/tarball/1.2',
+    download_url='https://github.com/fabienvauchelles/scrapoxy-python-api/tarball/1.3',
     keywords=['scrapoxy', 'scrapy', 'scraper', 'scraping'],
     classifiers=[],
 )
