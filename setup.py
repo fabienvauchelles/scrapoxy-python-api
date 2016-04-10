9ba89e8948ef771e27db0da97b379671c3fa42bc
Fabien Vauchelles <fabien@vauchelles.com>
2016-04-10 19:50:54 +0200

    feat(release): add 1.5 release config with install_requires

--- setup.py
+++ setup.py
@@ -7,12 +7,13 @@ except ImportError:
 setup(
     name='scrapoxy',
     packages=find_packages(),
-    version='1.4',
+    install_requires=['requests'],
+    version='1.5',
     description='Use Scrapoxy with Scrapy',
     author='Fabien Vauchelles',
     author_email='fabien@vauchelles.com',
     url='https://github.com/fabienvauchelles/scrapoxy-python-api',
-    download_url='https://github.com/fabienvauchelles/scrapoxy-python-api/tarball/1.4',
+    download_url='https://github.com/fabienvauchelles/scrapoxy-python-api/tarball/1.5',
     keywords=['crawler', 'crawling', 'scrapoxy', 'scrapy', 'scraper', 'scraping'],
     classifiers=[],
 )
